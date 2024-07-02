6148f57131837ee2e3b09a659dfa30abdea4e2a4
Nathalya <nathalya.gsn@gmail.com>
2024-07-02 18:24:48 -0300

    first commit

---
+++ sabor-express/modelos/musica.py
@@ -0,0 +1,11 @@
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = ''
+
+musica1 = Musica()
+musica1.nome = 'blablabla'
+musica1.artista = 'blablabla'
+musica1.duracao = '0800'
+
+print(f'Música: {musica1.nome} - Banda: {musica1.artista} - {musica1.duracao} segundos')
